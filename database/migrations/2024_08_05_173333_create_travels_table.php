3f3f351eed24b73f8e650c0d520627aee67369bb
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-08-06 17:18:56 +0200

    fix tables

--- database/migrations/2024_08_05_173333_create_travels_table.php
+++ database/migrations/2024_08_05_173333_create_travels_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->string('slug', 150);
+            $table->string('slug', 150)->nullable();
             $table->string('cover_img')->nullable();
             $table->text('description')->nullable();
             $table->date('start_date');
